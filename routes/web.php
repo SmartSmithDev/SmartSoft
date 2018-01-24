fc0f8546a5c6de52a52dda41be8ce6408e8b3bce
Vinay Bansal Mac <vinay250493@gmail.com>
2018-01-24 11:18:50 +0530

    intend

--- routes/web.php
+++ routes/web.php
@@ -27,21 +27,21 @@ Route::group(['prefix' => 'taxes'], function () {
 });
 
 Route::group(['prefix' => 'sales'], function () {
-Route::resource('sales', 'Sales\Sales');
-Route::get('/autofill','Sales\Sales@autoFill');
-Route::resource('customers','Sales\Customers');
-Route::post('/customerInfo','Sales\Sales@customerInfo');
-Route::post('customerajax','Sales\Customers@store1');
-Route::post('invoice_order_check','Sales\Sales@checkExist');
-Route::resource('sales/payments','Sales\Payments');
-Route::get('/quantity','Sales\Sales@quantity');
+	Route::resource('sales', 'Sales\Sales');
+	Route::get('/autofill','Sales\Sales@autoFill');
+	Route::resource('customers','Sales\Customers');
+	Route::post('/customerInfo','Sales\Sales@customerInfo');
+	Route::post('customerajax','Sales\Customers@store1');
+	Route::post('invoice_order_check','Sales\Sales@checkExist');
+	Route::resource('sales/payments','Sales\Payments');
+	Route::get('/quantity','Sales\Sales@quantity');
 });
 
 
 Route::group(['prefix' => 'purchases'], function () {
-Route::resource('vendors','Purchases\Vendors');
-Route::resource('payments','Purchases\Payments');
-Route::resource('Purchases','Purchases\Purchases');
+	Route::resource('vendors','Purchases\Vendors');
+	Route::resource('payments','Purchases\Payments');
+	Route::resource('Purchases','Purchases\Purchases');
 });
 
 Route::group(['prefix' => 'items'], function () {
