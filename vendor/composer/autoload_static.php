334ede8bdf3f6bdef34be713a89e5d536633c5ab
sshash13 <shashanksalian13@gmail.com>
2018-02-10 15:39:22 +0530

    no change

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -19,13 +19,13 @@ class ComposerStaticInit7195d8e53875225c12168c8341cb170a
         'decc78cc4436b1292c6c0d151b19445c' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
         'e7223560d890eab89cda23685e711e2c' => __DIR__ . '/..' . '/psy/psysh/src/Psy/functions.php',
+        '4db33613fce4c89aa2457a348853af0a' => __DIR__ . '/..' . '/grimthorr/laravel-user-settings/src/helpers.php',
         'e23faeee409e941dc9b4c80386209c39' => __DIR__ . '/..' . '/laracasts/flash/src/Laracasts/Flash/functions.php',
         'f0906e6318348a765ffb6eb24e0d0938' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
         '58571171fd5812e6e447dce228f52f4d' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Support/helpers.php',
         'f18cc91337d49233e5754e93f3ed9ec3' => __DIR__ . '/..' . '/laravelcollective/html/src/helpers.php',
         'd7354eb4c8441fbf08eec280b61c7c73' => __DIR__ . '/..' . '/rachidlaasri/laravel-installer/src/Helpers/functions.php',
         '377b22b161c09ed6e5152de788ca020a' => __DIR__ . '/..' . '/spatie/laravel-permission/src/helpers.php',
-        '4db33613fce4c89aa2457a348853af0a' => __DIR__ . '/..' . '/grimthorr/laravel-user-settings/src/helpers.php',
     );
 
     public static $prefixLengthsPsr4 = array (
