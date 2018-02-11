1b9504004dbacff2927d4862bde6d68e1d686a57
shriya2603 <sundriyalshriya@gmail.com>
2018-02-11 11:29:24 +0530

    nothing

--- SmartSoft/resources/views/partials/admin/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        Be Smart! Be You!
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright © 2018 <a href="http://www.smartsmith.tech">SMART SMITH</a>.</strong> All rights reserved.
-</footer>
